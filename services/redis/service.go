202d319efd2099049593c1a65d927cf7a8da0de0
Clement Rey <cr.rey.clement@gmail.com>
2017-05-09 17:50:11 +0200

    services:redis > updated implementation following Maestro's improvements

--- services/redis/service.go
+++ services/redis/service.go
@@ -16,6 +16,7 @@ package redis
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/garyburd/redigo/redis"
@@ -116,71 +117,28 @@ func New(p *redis.Pool) bandmaster.Service {
 // The given context defines the deadline for the above-mentionned operations.
 //
 //
-// NOTE1: Start is used by BandMaster's internal machinery, it shouldn't ever
-// have to be called by the end-user of the service.
-//
-// NOTE2: Start relies on the Maestro holding the service's base lock.
-func (s *Service) Start(
-	ctx context.Context, _ map[string]bandmaster.Service,
-) error {
-	errC := make(chan error, 1)
-	go func() {
-		defer close(errC)
-		c, err := s.pool.Dial()
-		if err != nil {
-			errC <- err
-			return
-		}
-		defer c.Close()
-		if _, err = c.Do("PING"); err != nil {
-			errC <- err
-			return
-		}
-	}()
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case err := <-errC:
-		if err != nil {
-			return err
-		}
+// Start is used by BandMaster's internal machinery, it shouldn't ever be called
+// directly by the end-user of the service.
+func (s *Service) Start(context.Context, map[string]bandmaster.Service) error {
+	fmt.Println("actual start")
+	c, err := s.pool.Dial()
+	if err != nil {
+		return err
 	}
-
+	defer c.Close()
+	if _, err = c.Do("PING"); err != nil {
+		return err
+	}
 	return nil
 }
 
-// Stop closes the underlying `redis.Pool`: if everything goes smoothly,
-// the service is marked as 'stopped'; otherwise, an error is returned.
+// Stop does nothing.
 //
-// The given context defines the deadline for the above-mentionned operations.
-//
-//
-// NOTE1: Stop is used by BandMaster's internal machinery, it shouldn't ever
-// have to be called by the end-user of the service.
-//
-// NOTE2: Stop relies on the Maestro holding the service's base lock.
-func (s *Service) Stop(ctx context.Context) error {
-	errC := make(chan error, 1)
-	go func() {
-		// If the context gets cancelled (unlikely), this routine will leak
-		// until the Close() call actually returns.
-		// We don't really care.
-		defer close(errC)
-		if err := s.pool.Close(); err != nil {
-			errC <- err
-			return
-		}
-	}()
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case err := <-errC:
-		if err != nil {
-			return err
-		}
-	}
-
+// Stop is used by BandMaster's internal machinery, it shouldn't ever be called
+// directly by the end-user of the service.
+func (s *Service) Stop(context.Context) error {
 	return nil
+	//return s.pool.Close()
 }
 
 // -----------------------------------------------------------------------------
